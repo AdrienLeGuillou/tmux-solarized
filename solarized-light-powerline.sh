15a8e824beb5bb8c522363855a0421a442368f0f
Ergys Dona <errikosd@gmail.com>
2019-07-17 20:36:35 +0200

    Use simpler colours.

--- solarized-light-powerline.sh
+++ solarized-light-powerline.sh
@@ -16,19 +16,19 @@ set -g status-style fg=colour245,bg=white
 # Left side of status bar
 set -g status-left-style fg=colour243,bg=white
 set -g status-left-length 40
-set -g status-left "#[fg=white,bg=colour245,nobold] #S #[fg=colour245,bg=white]"
+set -g status-left "#[fg=white,bg=colour11,nobold] #S #[fg=colour11,bg=white]"
 
 # Right side of status bar
 set -g status-right-style fg=colour243,bg=white
 set -g status-right-length 150
-set -g status-right "#[fg=colour245,bg=white]#[fg=white,bg=colour245] %H:%M #[fg=white,bg=colour245]#[fg=colour245,bg=white] %d-%b-%y #[fg=white,bg=colour245]"
+set -g status-right "#[fg=colour11,bg=white]#[fg=white,bg=colour11] %H:%M #[fg=white,bg=colour11]#[fg=colour11,bg=white] %d-%b-%y #[fg=white,bg=colour11]"
 
 # Window status
 set -g window-status-format "  #I #W  "
-set -g window-status-current-format "#[fg=white,bg=colour32]#[fg=white,nobold] #I #W #[fg=colour32,bg=white,nobold]"
+set -g window-status-current-format "#[fg=white,bg=colour14]#[fg=white,nobold] #I #W #[fg=colour14,bg=white,nobold]"
 
 # Current window status
-set -g window-status-current-style fg=white,bg=colour32
+set -g window-status-current-style fg=white,bg=colour14
 
 # Window with activity status
 set -g window-status-activity-style fg=white,bg=colour243
